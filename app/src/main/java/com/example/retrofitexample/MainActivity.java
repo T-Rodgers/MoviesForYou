ef2e874dedd4d57f0ab2b4f4d4fead0b8e091b98
Terrence Rodgers <tdr0082@gmail.com>
2020-10-27 15:57:47 -0500

    Initial commit

---
+++ app/src/main/java/com/example/retrofitexample/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.retrofitexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String API_KEY = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
