c4d17d28b9820f4705e547c3485123276d0e5be7
Terrence Rodgers <tdr0082@gmail.com>
2021-01-15 12:53:57 -0600

    Rename package and project

--- app/src/main/java/com/example/retrofitexample/utils/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.example.retrofitexample.utils;
-
-public class Constants {
-    public static final String API_KEY = "";
-    public static final String POPULARITY_DESC = "popularity.desc";
-    public static final String EXTRA_SELECTED_GENRE_ID = "selected_genre_id";
-    // Image is 342 x 513
-    public static final String IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w342";
-}
