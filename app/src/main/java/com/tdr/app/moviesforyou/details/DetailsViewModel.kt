34d01f49e8438354cf6249afd9abae3b2d1a8cde
Terrence Rodgers <tdr0082@gmail.com>
2021-11-02 23:05:37 -0500

    Create viewModel and factory

--- app/src/main/java/com/tdr/app/moviesforyou/details/DetailsViewModel.kt
+++ app/src/main/java/com/tdr/app/moviesforyou/details/DetailsViewModel.kt
@@ -1,4 +1,18 @@
 package com.tdr.app.moviesforyou.details
 
-class DetailsViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.tdr.app.moviesforyou.network.Movie
+
+class DetailsViewModel(movie: Movie, application: Application) : AndroidViewModel(application) {
+
+    private val _selectedMovie = MutableLiveData<Movie>()
+    val selectedMovie: LiveData<Movie>
+        get() = _selectedMovie
+
+    init {
+        _selectedMovie.value = movie
+    }
 }
---
+++ app/src/main/java/com/tdr/app/moviesforyou/details/DetailsViewModelFactory.kt
@@ -0,0 +1,17 @@
+package com.tdr.app.moviesforyou.details
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.tdr.app.moviesforyou.network.Movie
+
+class DetailsViewModelFactory(private val movie: Movie,
+                              private val application: Application): ViewModelProvider.Factory {
+
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(DetailsViewModel::class.java)) {
+                return DetailsViewModel(movie, application) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
+}
