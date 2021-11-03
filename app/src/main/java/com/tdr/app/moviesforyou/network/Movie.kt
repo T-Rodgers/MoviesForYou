147543cfb00753d3a7ccf09aa17f370a3545481a
Terrence Rodgers <tdr0082@gmail.com>
2021-11-02 22:40:19 -0500

    Add backdrop and poster properties, with BindingAdapters. Modify list_item.xml to contain only a movie poster

--- app/src/main/java/com/tdr/app/moviesforyou/network/Movie.kt
+++ app/src/main/java/com/tdr/app/moviesforyou/network/Movie.kt
@@ -1,9 +1,16 @@
 package com.tdr.app.moviesforyou.network
 
 import android.os.Parcelable
+import com.squareup.moshi.Json
 import kotlinx.parcelize.Parcelize
 
 data class MoviesResponse(val results: List<Movie>)
 
 @Parcelize
-data class Movie(val id: Int, val title: String, val overview: String) : Parcelable
+data class Movie(
+    val id: Int,
+    val title: String,
+    val overview: String,
+    @Json(name = "backdrop_path") val backdropPath: String,
+    @Json(name = "poster_path") val posterPath: String
+) : Parcelable
--- app/src/main/java/com/tdr/app/moviesforyou/utils/BindingUtils.kt
+++ app/src/main/java/com/tdr/app/moviesforyou/utils/BindingUtils.kt
@@ -8,10 +8,15 @@ import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.tdr.app.moviesforyou.R
 import com.tdr.app.moviesforyou.adapters.MovieListAdapter
 import com.tdr.app.moviesforyou.network.Movie
 import com.tdr.app.moviesforyou.network.MoviesApiStatus
+
+
+private const val BASE_BACKDROP_PATH_URL = "https://image.tmdb.org/t/p/w780" // 780x439
+private const val BASE_POSTER_PATH_URL = "https://image.tmdb.org/t/p/w300" // 300x450
 
 @BindingAdapter("movieId")
 fun TextView.setMovieId(movie: Movie?) {
@@ -72,6 +77,24 @@ fun bindProgress(progress: ProgressBar, status: MoviesApiStatus?) {
             progress.visibility = GONE
 
         }
+    }
+}
+
+@BindingAdapter("posterImage")
+fun bindPostImage(posterImageView: ImageView, movie: Movie?) {
+    movie?.let {
+        Glide.with(posterImageView.context)
+            .load(BASE_POSTER_PATH_URL + movie.posterPath)
+            .into(posterImageView)
+    }
+}
+
+@BindingAdapter("backdropImage")
+fun bindBackdropPhoto(backdropImageView: ImageView, movie: Movie?) {
+    movie?.let {
+        Glide.with(backdropImageView.context)
+            .load(BASE_BACKDROP_PATH_URL + movie.backdropPath)
+            .into(backdropImageView)
     }
 }
 
