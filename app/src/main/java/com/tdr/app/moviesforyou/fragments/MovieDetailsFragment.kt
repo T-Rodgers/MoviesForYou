dccbc6864ce7243c674c0442ead0bbb50f1291f8
Terrence Rodgers <tdr0082@gmail.com>
2021-11-02 23:05:16 -0500

    Add databinding to detailsfragment

--- app/src/main/java/com/tdr/app/moviesforyou/fragments/MovieDetailsFragment.kt
+++ app/src/main/java/com/tdr/app/moviesforyou/fragments/MovieDetailsFragment.kt
@@ -1,19 +1,34 @@
 package com.tdr.app.moviesforyou.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.tdr.app.moviesforyou.R
+import com.tdr.app.moviesforyou.databinding.FragmentMovieDetailsBinding
+import com.tdr.app.moviesforyou.details.DetailsViewModel
+import com.tdr.app.moviesforyou.details.DetailsViewModelFactory
 
 class MovieDetailsFragment : Fragment() {
+
+    private lateinit var binding: FragmentMovieDetailsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie_details, container, false)
+        val application = requireNotNull(activity).application
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_movie_details, container, false)
+        binding.lifecycleOwner = this
+        val movie = MovieDetailsFragmentArgs.fromBundle(requireArguments()).selectedMovie
+        val factory = DetailsViewModelFactory(movie, application)
+
+        binding.viewModel = ViewModelProvider(this, factory).get(DetailsViewModel::class.java)
+
+        return binding.root
     }
 }
