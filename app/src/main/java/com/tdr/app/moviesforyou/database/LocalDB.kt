58401445d3f1fa1feeae3058c32b6208ae028aed
Terrence Rodgers <tdr0082@gmail.com>
2022-09-06 09:26:53 -0500

    Initial Commit

---
+++ app/src/main/java/com/tdr/app/moviesforyou/database/LocalDB.kt
@@ -0,0 +1,18 @@
+package com.tdr.app.moviesforyou.database
+
+import android.content.Context
+import androidx.room.Room
+
+
+/**
+ * Singleton Object for Database for dependency injection.
+ */
+object LocalDB {
+
+    fun createMovieDao(context: Context): MovieDao {
+        return Room.databaseBuilder(
+            context.applicationContext,
+            MovieDatabase::class.java, "movies.db"
+        ).build().movieDao()
+    }
+}
